52d0440896b3011a42c896d0d807c8574bf66687
Rohil Visariya <rohilvisariya69@gmail.com>
2020-10-03 17:06:00 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import Uploader from "./Uploader";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Uploader />
+      <hr />
+    </div>
+  );
+};
+
+export default App;
